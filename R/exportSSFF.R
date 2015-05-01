b6e3e4e1844fe920dd77670bb1ed7b8824218e5c
Richard Beare <Richard.Beare@gmail.com>
2015-05-01 10:03:19 +1000

    basic reading done - onto checking

--- R/exportSSFF.R
+++ R/exportSSFF.R
@@ -1,11 +1,11 @@
 #' Convert the text file containing spline data to ssff
 #' 
-exportSplinesSSFF <- function(txtfile=NULL)
+exportSplinesSSFF <- function(txtfile=NULL, missingAsZero=TRUE)
     {
         if (is.null(txtfile)) {
             txtfile <- file.choose()
         }
-
+        splinepts <- 42
         raw <- readLines(txtfile)
         hd <- strsplit(raw[1], "\\t")[[1]]
         expected <- c("Client Surname", "Prompt","Time of sample in recording", "X,Y values of spline \"Tongue\" relative to \"bite plane\" (rotated)",
@@ -17,12 +17,42 @@ exportSplinesSSFF <- function(txtfile=NULL)
 
         raw <- raw[-1]
         raw <- strsplit(raw, "\\t")
-
+        speaker <- raw[[1]][1]
         ## Should be 42 spline points - 84 numbers - followed by 42 conf scores
         prompts <- sapply(raw, "[", 2)
         times <- as.numeric(sapply(raw, "[", 3))
-        rest <- lapply(raw, function(p)as.numeric(p[-(1:3)]))
+        rest <- lapply(raw, function(p){
+          g<-as.numeric(p[-(1:3)])
+          if (length(g)==125) {
+            g <- c(g, NA)
+          }
+          return(g)
+        })
+        
+        
+        if (missingAsZero) {
+          rest <- lapply(rest, function(M){
+            M[is.na(M)] <- 0
+            return(M)
+          })
+        }
+        rest <- unlist(rest)
+        dim(rest) <- c(3*splinepts, length(prompts))
+
+        splines <- rest[1:(2*splinepts),]
+        conf <- rest[(2*splinepts + 1):(3*splinepts),]
+        
+        ## Figure out the rows for each prompt
+        ## split returns indexes in order
+        promptrows <- split(1:length(prompts), prompts)
+        
+        ## Check for irregular spacing - need to produce a list of rows to ignore
+        samplerate <- median(diff(times))
+        timesperprompt <- lapply(promptrows, function(G){return(times[G])})
+        difftimes <- lapply(timesperprompt, function(J)return(abs(diff(J) - samplerate)))
         browser()
-        return(raw)
+        ## Now we figure out the output names
+        ## Format is lastname_firstname_word
+        return(prompts)
     }
         
