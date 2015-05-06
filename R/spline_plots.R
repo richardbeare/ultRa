b677c881006e49450660c03a71ae880cf805a1ec
Richard Beare <Richard.Beare@gmail.com>
2015-05-06 22:09:04 +1000

    updated column order for plotting

--- R/spline_plots.R
+++ R/spline_plots.R
@@ -4,10 +4,14 @@
 plot1 <- function(trackdata)
 {
   d <- trackdata$data
-  X <- t(d[,seq(from=1, by=2, length=ncol(d)/2)])
-  Y <- t(d[,seq(from=2, by=2, length=ncol(d)/2)])
+  lastX <- ncol(d)/3
+  lasty <- 2*ncol(d)/3
+  
+  X <- t(d[,1:lastx])
+  Y <- t(d[,(lastx+1):lasty])
+  cert <- t(d[,(lasty+1):ncol(d)])
   Time <- rep(as.numeric(rownames(d)), rep(ncol(d)/2, nrow(d)))
   samp <- rep(1:nrow(d), rep(ncol(d)/2, nrow(d)))
-  df <- data.frame(time=Time, X=as.vector(X), Y=as.vector(Y), sample=samp)
+  df <- data.frame(time=Time, X=as.vector(X), Y=as.vector(Y), sample=samp, certainty=cert)
   return(df)
 }
